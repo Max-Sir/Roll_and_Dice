05999a0607a6304b1c745e53084851143ee48bd8
Maxim <syromolotov.maks@mail.ru>
2021-08-12 00:33:23 +0300

    uploaded pet-project major files

--- app/src/main/java/com/coherentsolutions/max/sir/myapplication/MainActivity.kt
+++ app/src/main/java/com/coherentsolutions/max/sir/myapplication/MainActivity.kt
@@ -2,21 +2,54 @@ package com.coherentsolutions.max.sir.myapplication
 
 import android.graphics.Color
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.databinding.DataBindingUtil
+import com.coherentsolutions.max.sir.myapplication.databinding.ActivityMainBinding
+import java.util.logging.Level
+import java.util.logging.LogRecord
+import java.util.logging.Logger
+import kotlin.math.abs
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val rollButton = findViewById<Button>(R.id.main_button)
-        rollButton.setOnClickListener {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.mainButton.setOnClickListener {
             roll()
+
         }
+        binding.mainConstraintLayout.setOnClickListener { roll() }
+
+    }
+
+    private fun roll() {
+        val img = binding.imageView
+        val unpredictableNumber = Random.nextInt(1, 7)
+        map[unpredictableNumber]?.let { img.setImageResource(it) }
+        val logger=Logger.getLogger("Debug")
+        logger.log(Level.INFO,"given $unpredictableNumber")
+        Toast.makeText(this, "rolled dice number is $unpredictableNumber", Toast.LENGTH_SHORT)
+            .show()
+        binding.mainConstraintLayout.setBackgroundColor(
+            Color.parseColor(
+                "#${
+                    createRandomIntegerString(
+                        0x08
+                    )
+                }"
+            )
+        )
+        binding.mainButton.setBackgroundColor(Color.parseColor("#${createRandomIntegerString(0x08)}"))
+        binding.invalidateAll()
     }
 
 
@@ -30,21 +63,13 @@ class MainActivity : AppCompatActivity() {
     )
 }
 
-private fun MainActivity.roll() {
-    val img=findViewById<ImageView>(R.id.imageView)
-    val unpredictableNumber=Random.nextInt(1, 7)
-    map[unpredictableNumber]?.let { img.setImageResource(it) }
-    Toast.makeText(this,"rolled dice number is $unpredictableNumber",Toast.LENGTH_SHORT).show()
-    findViewById<ConstraintLayout>(R.id.mainConstraintLayout).setBackgroundColor(Color.parseColor("#${createRandomIntegerString(0x08)}"))
-    findViewById<Button>(R.id.main_button).setBackgroundColor(Color.parseColor("#${createRandomIntegerString(0x08)}"))
-}
 
 internal fun createRandomIntegerString(length: Int): String {
     val stringBuilder = StringBuilder()
     val hexsInterpolation = "0123456789abcdef"
     for (i in 0 until length) {
         val tmpInt = Random.nextInt()
-        val x = Math.abs(tmpInt) % 16
+        val x = abs(tmpInt) % 16
         stringBuilder.append(hexsInterpolation[x])
     }
     return stringBuilder.toString()
